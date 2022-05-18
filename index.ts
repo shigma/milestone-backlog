31fc451fca93fcfb43484886b3603f593b234b44
H4M5TER <zhuxx2003@gmail.com>
2022-05-19 02:34:52 +0800

    refac
    feat: #3

--- index.ts
+++ index.ts
@@ -9,20 +9,14 @@ const label = core.getInput('label')
 
 const octokit = github.getOctokit(token)
 const { payload, repo } = github.context
-const { action, issue, milestone } = payload
+const { action, issue } = payload
+const milestone = payload.milestone ?? issue.milestone
 
-async function run() {
-  if (action !== 'milestoned' && action !== 'demilestoned') {
-    core.error('Invalid event ' + action)
+async function runIssue() {
+  if (!['milestoned', 'demilestoned'].includes(action)) {
+    core.error('Invalid event for issue: ' + action)
     return
   }
-  if (issue.labels.find((v: { name: string }) => v.name === label)) {
-    core.info('This is already a plan.')
-    return
-  }
-
-  core.info('action: ' + action)
-
   const filter = {
     ...repo,
     creator: creator,
@@ -41,50 +35,83 @@ async function run() {
     plan.body.match(new RegExp(`(.*${header}\\s*)((?:- [^-\\r\\n]+\\r?\\n)*)(.*)`, 's'))
   core.info(`above: ${JSON.stringify(above)}\nbacklog: ${JSON.stringify(backlog)}\nbelow: ${JSON.stringify(below)}`)
 
-  let new_backlog = backlog.replace(/\r\n/g, '\n')
-  if (action === 'demilestoned') {
-    const regex = new RegExp(`^- \\[[ x]\\] #${issue.number}\\s+`, 'm')
-    if (new_backlog.match(regex)) {
-      core.info('removed')
-      new_backlog = new_backlog.replace(regex, '')
-    } else {
-      core.info('not existing or modified')
-    }
-  } else {
-    if (new_backlog.includes(`#${issue.number}`)) {
-      core.info('already exists')
-    } else {
-      const backlogs = new_backlog.trim().split('\n')
-      const newline = `- [${issue.state === 'open' ? ' ' : 'x'}] #${issue.number} `
-      core.info(`backlogs: ${JSON.stringify(backlogs)}\nnewline: ${newline}`)
-      const id = backlogs.findIndex((v) => {
-        const match = v.match(/#(\d+)/)
-        if (match)
-          if (parseInt(match[1]) > issue.number)
-            return true
-        return false
-      })
-      if (id === -1) {
-        backlogs.push(newline)
-        core.info('appended to the end')
+  let new_backlog = backlog.replace(/\r\n/g, '\n'), regex
+  switch (action) {
+    case 'demilestoned':
+      regex = new RegExp(`^- \\[[ x]\\] #${issue.number}\\s+`, 'm')
+      if (new_backlog.match(regex)) {
+        core.info('removed')
+        new_backlog = new_backlog.replace(regex, '')
       } else {
-        core.info('insert before: ' + backlogs[id])
-        backlogs[id] = newline + '\n' + backlogs[id] // splice have edging case
+        core.info('not existing or modified')
       }
-      new_backlog = '\n' + backlogs.join('\n') + '\n'
-    }
+      break
+    case 'milestoned':
+      if (new_backlog.includes(`#${issue.number}`)) {
+        core.info('already exists')
+      } else {
+        const backlogs = new_backlog.trim().split('\n')
+        const newline = `- [${issue.state === 'open' ? ' ' : 'x'}] #${issue.number} `
+        core.info(`backlogs: ${JSON.stringify(backlogs)}\nnewline: ${newline}`)
+        const id = backlogs.findIndex((v) => {
+          const match = v.match(/#(\d+)/)
+          if (match)
+            if (parseInt(match[1]) > issue.number)
+              return true
+          return false
+        })
+        if (id === -1) {
+          backlogs.push(newline)
+          core.info('appended to the end')
+        } else {
+          core.info('insert before: ' + backlogs[id])
+          backlogs[id] = newline + '\n' + backlogs[id] // splice have edging case
+        }
+        new_backlog = '\n' + backlogs.join('\n') + '\n'
+      }
+      break
+    default:
+      break
   }
   core.info('new: ' + JSON.stringify(new_backlog))
   const resp = await octokit.rest.issues.update({
     ...repo,
     issue_number: plan.number,
-    body: above + new_backlog + below
+    body: above.trimEnd() + '\n' + new_backlog + below
   })
   core.info('resp:\n' + JSON.stringify(resp))
 }
 
+async function runPlan() {
+  switch (action) {
+    case 'closed':
+    case 'reopened':
+      const resp = await octokit.rest.issues.updateMilestone({
+        ...repo,
+        milestone_number: milestone.number,
+        state: issue.state
+      })
+      core.info(JSON.stringify(resp))
+      break
+    default:
+      core.error('Invalid event for plan: ' + action)
+      break
+  }
+}
+
 try {
-  run()
+  core.startGroup('context')
+  core.info(JSON.stringify(github.context))
+  core.endGroup()
+  core.info('action: ' + action)
+  if (!milestone) { }
+  else if (issue.labels.find((v: { name: string }) => v.name === label)) {
+    core.info('plan triggered')
+    runPlan()
+  }
+  else {
+    runIssue()
+  }
 } catch (error) {
   core.error(JSON.stringify(error))
 }
